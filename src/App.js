58fb58960c0590f1a3315f16edce0c39a6b07440
Gautham Chengappa <gautham.hc@gmail.com>
2021-02-25 16:01:41 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './App.css';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import "weather-icons/css/weather-icons.css";
+
+//importing components
+import Tempapp from './app_component/Tempapp'
+
+
+function App() {
+
+  return (
+    <Tempapp/>
+  );
+}
+
+export default App;
---
+++ src/app_component/Tempapp.jsx
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react'
+
+const Tempapp = () => {
+  // const API_key = "b011dddbe6d7e70e60089fbbf4ecbc87";
+  const [city, setCity] = useState(null);
+  console.log(city);
+  const [search, setSearch] = useState("Bengaluru");
+
+  const cityName = (e) => {
+    setSearch(e.target.value)
+  }
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=b011dddbe6d7e70e60089fbbf4ecbc87`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setCity(data.main);
+      // console.log(setCity);
+    }
+    fetchApi();
+  }, [search])
+  
+  return(
+    <>
+      <div className="box">
+        <div className="inputData">
+          <input className="inputField" type="search" value={search} onChange={cityName}  />
+        </div>
+        {!city ? (
+         <p className="errorMsg">No Data Found</p>
+        ) : (
+        <div>
+          <div className="info">
+            <h2 className="location">
+            <i className="fas fa-street-view">{search}</i>
+            </h2>
+            <h1 className="temp">{city.temp}&deg;cel</h1>
+            <h3 className="tempmin_max"> Min : {city.temp_min}&deg; cel | Max : {city.temp_max}&deg; cel </h3>
+          </div>
+        </div>
+        )}
+      </div>
+    </>
+  )
+}
+
+export default Tempapp
+
